8af4a90a89be62842b00add851eef7f5f68b35f6
Ravi Teja <ravitejathumnoori@gmail.com>
2023-08-09 16:11:50 +0530

    first commit

---
+++ src/main/java/com/messenger/home/messenger/resources/MessengerResource.java
@@ -0,0 +1,25 @@
+package com.messenger.home.messenger.resources;
+
+import java.util.List;
+
+import com.messenger.home.messenger.model.Message;
+import com.messenger.home.messenger.service.MessageService;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("messages")
+public class MessengerResource {
+	
+	
+	MessageService messageService = new MessageService();
+	
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Message> getMessages() {
+        return messageService.getAllMessages();
+    }
+
+}
